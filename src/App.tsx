589e1e286ee8997d641bdd1a0e402e8da68ab9d1
karolyne <karoldm1579@gmail.com>
2021-12-24 18:18:10 -0300

    Dia 02

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,21 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import { Home } from './pages/Home';
+import { NewRoom } from './pages/NewRoom';
+
+import { AuthContextProvider } from './contexts/AuthContext';
+
 function App() {
+
   return (
-    <h1>Hello</h1>
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/rooms/new' element={<NewRoom />} />
+        </Routes>
+      </AuthContextProvider>
+    </ BrowserRouter >
   );
 }
 
