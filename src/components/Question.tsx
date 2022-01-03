35e001e0c6ff88c4977d2693605ed6795cc64655
karolyne <karoldm1579@gmail.com>
2022-01-03 16:42:41 -0300

    Dia 04

---
+++ src/components/Question.tsx
@@ -0,0 +1,29 @@
+import { ReactNode } from "react";
+
+import { auth } from "../services/firebase";
+
+import '../styles/question.scss';
+
+type QuestionProps = {
+  content: string;
+  author: {
+    name: string;
+    avatar: string;
+  };
+  children?: ReactNode;
+}
+
+export function Question({ content, author, children }: QuestionProps) {
+  return (
+    <div className="question">
+      <p>{content}</p>
+      <footer>
+        <div className="user-info">
+          <img src={author.avatar} alt={auth.name} />
+          <span>{author.name}</span>
+        </div>
+        <div>{children}</div>
+      </footer>
+    </div>
+  );
+}
